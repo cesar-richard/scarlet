97f34e82c20fe8c48497ef9e38052b766ab01ee7
César Richard <cesar.richard@weezevent.com>
2022-06-13 18:30:46 +0200

    Pairing ok
    sign: Gulltoppr <cesar@crichard.fr>

--- src/Cards/MifareUltralight/MifareUltralightFastReadError.js
+++
@@ -1,8 +0,0 @@
-const { TransmitError } = require("nfc-pcsc");
-
-module.exports = class MifareUltralightFastReadError extends TransmitError {
-  constructor(code, message, previousError) {
-    super(code, message, previousError);
-    this.name = "MifareUltralightFastReadError";
-  }
-};
